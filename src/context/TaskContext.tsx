eb01af244deaea82ddd89155a245b71fa82b27ef
wellitonaraujo <wellitonaraujodev@hotmail.com>
2024-04-12 14:35:20 -0300

    Add Async Storage

--- src/context/TaskContext.tsx
+++ src/context/TaskContext.tsx
@@ -1,4 +1,5 @@
-import React, {createContext, useContext, useState} from 'react';
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface Task {
   title: string;
@@ -11,30 +12,55 @@ interface TaskContextType {
   tasks: Task[];
   addTask: (task: Task) => void;
   deleteTask: (index: number) => void;
-  updateTasks: (updatedTasks: Task[]) => void; // Método para atualizar as tarefas
+  updateTasks: (updatedTasks: Task[]) => void;
 }
 
 const TaskContext = createContext<TaskContextType>({
   tasks: [],
   addTask: () => {},
   deleteTask: () => {},
-  updateTasks: () => {}, // Inicializa como uma função vazia
+  updateTasks: () => {},
 });
 
 export const TaskProvider: React.FC = ({children}) => {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   const addTask = (task: Task) => {
-    setTasks([...tasks, task]);
+    const updatedTasks = [...tasks, task];
+    setTasks(updatedTasks);
+    AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks));
   };
 
+  // Ao recuperar tarefas do AsyncStorage
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const storedTasks = await AsyncStorage.getItem('tasks');
+        if (storedTasks) {
+          const parsedTasks: Task[] = JSON.parse(storedTasks).map(
+            (task: any) => ({
+              ...task,
+              date: new Date(task.date),
+            }),
+          );
+          setTasks(parsedTasks);
+        }
+      } catch (error) {
+        console.error('Error fetching tasks from AsyncStorage:', error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
   const deleteTask = (index: number) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
+    AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks));
   };
 
   const updateTasks = (updatedTasks: Task[]) => {
     setTasks(updatedTasks);
+    AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks));
   };
 
   return (
