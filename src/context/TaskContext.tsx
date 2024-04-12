c000adb29c1f268a30bef7df68b95183a701be2d
wellitonaraujo <wellitonaraujodev@hotmail.com>
2024-04-12 14:24:15 -0300

    fix deleting task

--- src/context/TaskContext.tsx
+++ src/context/TaskContext.tsx
@@ -1,41 +1,44 @@
-// TaskContext.tsx
-
 import React, {createContext, useContext, useState} from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-interface TaskType {
+interface Task {
   title: string;
   description: string;
   priority: 'low' | 'average' | 'high';
   date: Date;
-  isSelected: boolean;
 }
 
 interface TaskContextType {
-  tasks: TaskType[];
-  addTask: (task: TaskType) => void;
+  tasks: Task[];
+  addTask: (task: Task) => void;
+  deleteTask: (index: number) => void;
+  updateTasks: (updatedTasks: Task[]) => void; // Método para atualizar as tarefas
 }
 
 const TaskContext = createContext<TaskContextType>({
   tasks: [],
   addTask: () => {},
+  deleteTask: () => {},
+  updateTasks: () => {}, // Inicializa como uma função vazia
 });
 
 export const TaskProvider: React.FC = ({children}) => {
-  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-  const addTask = async (task: TaskType) => {
-    try {
-      const updatedTasks = [...tasks, task];
-      setTasks(updatedTasks);
-      await AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks));
-    } catch (error) {
-      console.error('Error adding task:', error);
-    }
+  const addTask = (task: Task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (index: number) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const updateTasks = (updatedTasks: Task[]) => {
+    setTasks(updatedTasks);
   };
 
   return (
-    <TaskContext.Provider value={{tasks, addTask}}>
+    <TaskContext.Provider value={{tasks, addTask, deleteTask, updateTasks}}>
       {children}
     </TaskContext.Provider>
   );
